cf84b208f3cdf7be6a104295f5c1d521823cd33f
kyle nosar <kylenosar@gmail.com>
2019-04-02 15:51:03 -0700

    added render

--- src/main.rs
+++ src/main.rs
@@ -58,28 +58,48 @@ fn pixel_to_point(bounds:(usize, usize),
             pixel: (usize, usize),
             upper_left: Complex<f64>,
             lower_right: Complex<f64>) -> Complex<f64> {
-
-                let (width, height) = (lower_right.re - upper_left.re, 
+            let (width, height) = (lower_right.re - upper_left.re, 
                                 upper_left.im - lower_right.im);
 
-                Complex {
-                    re: upper_left.re +pixel.0 as f64 * width / bounds.0 as f64,
-                    im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64
-                    //subtraction here because pixel.1 increases as we go down,
-                    // but the imaginary component increases as we go up.
-                }
+            Complex {
+                re: upper_left.re +pixel.0 as f64 * width / bounds.0 as f64,
+                im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64
+                //subtraction here because pixel.1 increases as we go down,
+                // but the imaginary component increases as we go up.
             }
+}
 
+
+#[allow(dead_code)]
+fn render(pixels: &mut [u8],
+        bounds:(usize, usize),
+        upper_left: Complex<f64>,
+        lower_right: Complex<f64>)
+{
+    assert!(pixels.len() == bounds.0 * bounds.1);
+
+    for row in 0 .. bounds.0 {
+        for column in 0 .. bounds.0 {
+            let point = pixel_to_point(bounds, (column, row),
+                            upper_left, lower_right);
+
+            pixels[row * bounds.0 + column] = match escape_time(point, 255) {
+                None => 0,
+                Some(count) => 255 - count as u8
+            };
+        }
+    }
+}
 /// parse_pair test
 #[test]
 fn	test_parse_pair() {
-    assert_eq!(parse_pair::<i32>("",								','),	None);
-    assert_eq!(parse_pair::<i32>("10,",					','),	None);
-    assert_eq!(parse_pair::<i32>(",10",					','),	None);
-    assert_eq!(parse_pair::<i32>("10,20",			','),	Some((10,	20)));
-    assert_eq!(parse_pair::<i32>("10,20xy",	','),	None);
-    assert_eq!(parse_pair::<f64>("0.5x",				'x'),	None);
-    assert_eq!(parse_pair::<f64>("0.5x1.5",	'x'),	Some((0.5,	1.5)));
+    assert_eq!(parse_pair::<i32>("", ','), None);
+    assert_eq!(parse_pair::<i32>("10,",	','), None);
+    assert_eq!(parse_pair::<i32>(",10",	','), None);
+    assert_eq!(parse_pair::<i32>("10,20", ','),	Some((10,	20)));
+    assert_eq!(parse_pair::<i32>("10,20xy",	','), None);
+    assert_eq!(parse_pair::<f64>("0.5x", 'x'),	None);
+    assert_eq!(parse_pair::<f64>("0.5x1.5",	'x'), Some((0.5,	1.5)));
 }
 
 /// parse_complex test
@@ -89,12 +109,13 @@ fn	test_parse_complex() {
     assert_eq!(parse_complex(",-0.0625"),	None);
 }
 
+/// pixel_to_point test
 #[test]
 fn test_pixel_to_point() {
     assert_eq!(pixel_to_point((100,100), (25, 75),
                     Complex { re: -1.0, im: 1.0 },
                     Complex { re: 1.0, im: -1.0 }),
-            Complex { re: -0.5, im: -0.5 });
+                    Complex { re: -0.5, im: -0.5 });
 }
 
 fn main() {
